e7542cdecae3f45112133e4e1cd0d23efdd34de2
mbahgojol <profghozimahdi@gmail.com>
2025-01-28 23:26:32 +0700

    feat: create exception handler

--- backend/src/data/datasource/note.datasource.ts
+++ backend/src/data/datasource/note.datasource.ts
@@ -7,60 +7,41 @@ class NoteDatasource {
   constructor(@inject(AppDatabase) private appDb: AppDatabase) {}
 
   async getAllNotes(): Promise<NoteData[]> {
-    try {
-      return await this.appDb.noteDao().find({});
-    } catch (e) {
-      throw e;
-    }
+    return this.appDb.noteDao().find({});
   }
 
   async getNoteById(id: string): Promise<NoteData | null> {
-    try {
-      const note = await this.appDb.noteDao().findById(id);
-      if (!note) {
-        return null;
-      }
-
-      return note;
-    } catch (e) {
-      throw e;
+    const note = await this.appDb.noteDao().findById(id);
+    if (!note) {
+      return null;
     }
+
+    return note;
   }
 
   async createNote(newNote: Omit<NoteData, '_id'>): Promise<NoteData | null> {
-    try {
-      const data = {
-        ...newNote,
-        createdAt: new Date().toISOString(),
-      };
-      return await this.appDb.noteDao().create(data);
-    } catch (e) {
-      throw e;
-    }
+    const data = {
+      ...newNote,
+      createdAt: new Date().toISOString(),
+    };
+
+    return await this.appDb.noteDao().create(data);
   }
 
   async updateNote(noteData: NoteData): Promise<NoteData | null> {
-    try {
-      return await this.appDb.noteDao().findByIdAndUpdate(
-        noteData._id,
-        noteData,
-        {
-          runValidators: true,
-          new: true
-        }
-      );
-    } catch (e) {
-      throw e;
-    }
+    return this.appDb.noteDao().findByIdAndUpdate(
+      noteData._id,
+      noteData,
+      {
+        runValidators: true,
+        new: true
+      }
+    );
   }
 
   async deleteNote(id: string): Promise<boolean> {
-    try {
-      const result = await this.appDb.noteDao().findByIdAndDelete(id)
-      return result != null;
-    } catch (e) {
-      throw e;
-    }
+    const result = await this.appDb.noteDao().findByIdAndDelete(id)
+    return result != null;
   }
 }
 
